a7cbc572ab7ea16aa67907e33d1196dd302d6061
Rhys <74600933+rhystmorgan@users.noreply.github.com>
2022-02-12 13:54:01 +0000

    Create Homework.hs
    Solution for Homework Assignment Week 04 Plutus Pioneers
    
    I have included my failed attempts to solve as well so you can see my thinking and processes as I figured this out

---
+++ week04/Homework.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE TypeApplications   #-}
+{-# LANGUAGE TypeOperators      #-}
+
+module Week04.Homework where
+
+import Data.Aeson            (FromJSON, ToJSON)
+import Data.Functor          (void)
+import Data.Text             (Text, unpack)
+import Data.Void             (Void)
+import GHC.Generics          (Generic)
+import Ledger
+import Ledger.Ada            as Ada
+import Ledger.Constraints    as Constraints
+import Plutus.Contract       as Contract
+import Plutus.Trace.Emulator as Emulator
+import Wallet.Emulator.Wallet
+import Control.Monad.Freer.Extras as Extras
+
+data PayParams = PayParams
+    { ppRecipient :: PaymentPubKeyHash
+    , ppLovelace  :: Integer
+    } deriving (Show, Generic, FromJSON, ToJSON)
+
+type PaySchema = Endpoint "pay" PayParams
+
+payContract :: Contract () PaySchema Text ()
+payContract = do
+    pp <- awaitPromise $ endpoint @"pay" return
+    let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
+    handleError (\err -> Contract.logError $ "caught: " ++ unpack err) $ void $ submitTx tx
+    payContract
+
+--handleError (\err -> Contract.logError $ "caught: " ++ unpack err)
+{-
+payContract :: Contract () PaySchema Text ()
+payContract = do
+    pp <- awaitPromise $ endpoint @"pay" return
+    let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
+    void $ submitTx tx
+    handleError (\err -> Contract.logError $ "caught: " ++ unpack err)
+    payContract
+-}
+
+{-
+payContract :: Contract () PaySchema Text ()
+payContract = do
+    pp <- awaitPromise $ endpoint @"pay" return
+    let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
+    succ <- submitTx tx
+    if not succ
+        then Contract.throwError "BOOM!"
+        else return ()
+    payContract
+-}
+
+-- A trace that invokes the pay endpoint of payContract on Wallet 1 twice, each time with Wallet 2 as
+-- recipient, but with amounts given by the two arguments. There should be a delay of one slot
+-- after each endpoint call.
+
+
+payTrace :: Integer -> Integer -> EmulatorTrace ()
+payTrace a b = do
+    h1 <- activateContractWallet (knownWallet 1) payContract
+    callEndpoint @"pay" h1 $ PayParams
+        { ppRecipient = mockWalletPaymentPubKeyHash $ knownWallet 2
+        , ppLovelace = a
+        }
+    void $ Emulator.waitNSlots 1
+    callEndpoint @"pay" h1 $ PayParams
+        { ppRecipient = mockWalletPaymentPubKeyHash $ knownWallet 2
+        , ppLovelace = b 
+        }
+    void $ Emulator.waitNSlots 2
+    
+    
+{-    
+    h1 <- activateContractWallet (knownWallet 1) payContract
+    h2 <- activateContractWallet (knownWallet 2) payContract
+    void $ Emulator.waitNSlots 1
+    callEndpoint @"pay" h1 $ PayParams a
+    void $ Emulator.waitNSlots 1
+    callEndpoint @"pay" h1 $ PayParams bs
+    void $ Emulator.waitNSlots 2
+-}
+
+    
+{-    
+    callEndpoint @"pay" h1 a
+    void $ waitNSlots 1
+    callEndpoint @"pay" h1 b 
+-}    
+
+payTest1 :: IO ()
+payTest1 = runEmulatorTraceIO $ payTrace 10_000_000 20_000_000
+
+payTest2 :: IO ()
+payTest2 = runEmulatorTraceIO $ payTrace 1000_000_000 20_000_000
